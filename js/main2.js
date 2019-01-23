19b1592a7d8cecf849879d1d00351ff0d2f6734a
jaclynflomen <j_floman@fanshaweonline.ca>
2019-01-23 12:07:14 -0500

    week 3

--- js/main2.js
+++ js/main2.js
@@ -8,9 +8,19 @@ const UsersPageComponent = {
     template: "<h2>You're on the users page</h2>"
 };
 
+const ContactPageComponent = {
+    template: "<h2>You're on the contact page</h2>"
+};
+
+const ErrorPageComponent = {
+    template: "<h2>Page not found! Please try again.</h2>"
+};
+
 const routes = [
     { path: '/', name: 'home', component: HomePageComponent },
-    { path: '/users', name: 'users', component: UsersPageComponent}
+    { path: '/users', name: 'users', component: UsersPageComponent},
+    { path: '/contact', name: 'contact', component: ContactPageComponent},
+    { path: '/*', name: 'error', component: ErrorPageComponent }
 ];
 
 const router = new VueRouter ({
